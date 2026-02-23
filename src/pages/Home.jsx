17077d4c11bcf84842073da1a15672cf28798ed9
Amit Sahoo <amitkusahoo1212@gmail.com>
2026-02-23 12:08:51 +0530

    fotter change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,11 +11,9 @@ function Home() {
             <ComponentGrid />
 
             <footer style={{ marginTop: '50px', borderTop: '1px solid var(--border)', padding: '50px 20px', textAlign: 'center', color: 'var(--secondary-text)' }}>
-                <p>© 2026 BennyUI. All Rights are reserved.</p>
                 <p>Crafted with BennyUI by Amit</p>
                 <p>Simple • Fast • Elegant — BennyUI</p>
-
-                <h3>If anyone is interested in using or maintaining this for their side project, feel free to message me on LinkedIn.</h3>
+                <p>© 2026 BennyUI. All Rights are reserved.</p> 
             </footer>
         </div>
     );
